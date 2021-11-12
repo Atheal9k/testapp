37e50545c3419713a59393d04171c6019cf04988
Victor <v.huang435@gmail.com>
2021-11-12 19:14:53 +1100

    new

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React NOWOWOW
+          TROLLLLIGGOLGDOGDLGODRLGO
         </a>
       </header>
     </div>
